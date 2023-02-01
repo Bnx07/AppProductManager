8930bfdbfe9af8f0312e252c8405323c22919fa6
Bnx07 <benjabastan@gmail.com>
2023-02-01 14:36:27 -0300

    Finalizacion diseño no responsive

---
+++ src/js/editProduct.js
@@ -0,0 +1,6 @@
+const product = {
+    id: 0,
+    title: "Espejo",
+    price: 400.50
+}
+
--- src/js/newTransaction.js
+++ src/js/newTransaction.js
@@ -20,5 +20,5 @@ const addTransaction = () => {
             }
         }
     }
-    console.log(products)
+    console.log(products);
 }
